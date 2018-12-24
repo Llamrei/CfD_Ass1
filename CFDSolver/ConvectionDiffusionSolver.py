af0a3c8507653f51ffb6eff644d4e5436b0f2698
Llamrei <al3615@ic.ac.uk>
2018-12-24 17:16:23 +0000

    Improved handling of impossible analytical

--- CFDSolver/ConvectionDiffusionSolver.py
+++ CFDSolver/ConvectionDiffusionSolver.py
@@ -4,9 +4,11 @@ from CFDSolver.Tools import solveTDM
 
 """
 Future work:
+Implement hybrid discretisation of flux term
 Implement for solving of velocities also
 Implement general dimensionality
 Implement for transport of scalars
+Implement for unsteady flows
 Would be cool to write a diff eqn string parser that then discretizes
 """
 
@@ -24,7 +26,7 @@ class FlowProperties:
     default to 1 for density and diffusion - no default for velocity"""
 
     def __init__(
-        self, velocity: float, diffusion_coefficient: float = 1, density: float = 1
+        self, velocity: float, diffusion_coefficient: float = 0.5918, density: float = 1
     ):
         self.density = density
         self.diffusion_coefficient = diffusion_coefficient
@@ -47,6 +49,9 @@ class OneDimensionalConvectionDiffusionSystem:
         )
         self._delta = self.initial_phi_grid[-1] - self.initial_phi_grid[0]
         self._grid_size = self.real_length / (self.n - 1)
+        self._almost_flux = self.scalars.density * self.scalars.velocity
+        self._almost_diffusion = self.scalars.diffusion_coefficient / self._grid_size
+        self.cd_stable = abs(self._almost_flux / self._almost_diffusion) <= 2
 
     def _sol(self, i):
         # Calculates analytical answer at node i of self.n - primarily to make code neater
@@ -68,28 +73,26 @@ class OneDimensionalConvectionDiffusionSystem:
         diffusion_scheme: DifferencingScheme = DifferencingScheme.CENTRAL,
     ):
         """Solve system numerically using a_p * Phi_p = a_e * Phi_e + a_w * Phi_w"""
-        normalized_flux = self.scalars.density * self.scalars.velocity
-        normalized_diffusion = self.scalars.diffusion_coefficient / self._grid_size
         if diffusion_scheme != DifferencingScheme.CENTRAL:
             # In future could implement other differencing schemes on the diffusion element too
             raise NotImplementedError
         if convection_scheme == DifferencingScheme.CENTRAL:
-            a_e = normalized_diffusion - 0.5 * normalized_flux
-            a_w = normalized_diffusion + 0.5 * normalized_flux
+            a_e = self._almost_diffusion - 0.5 * self._almost_flux
+            a_w = self._almost_diffusion + 0.5 * self._almost_flux
             # Density and velocity constant so in a_p last term cancels itself
             a_p = a_e + a_w
         elif convection_scheme == DifferencingScheme.UPWIND:
-            a_e = normalized_diffusion + max([-1 * normalized_flux, 0])
-            a_w = normalized_diffusion + max([1 * normalized_flux, 0])
+            a_e = self._almost_diffusion + max([-1 * self._almost_flux, 0])
+            a_w = self._almost_diffusion + max([1 * self._almost_flux, 0])
             # Density and velocity constant so in a_p last term cancels itself
             a_p = a_e + a_w
         elif convection_scheme == DifferencingScheme.POWER_LAW:
-            a_e = normalized_diffusion * max(
-                [(1 - 0.1 * abs(normalized_flux / normalized_diffusion)) ** 5, 0]
-            ) + max([-1 * normalized_flux, 0])
-            a_w = normalized_diffusion * max(
-                [(1 - 0.1 * abs(normalized_flux / normalized_diffusion)) ** 5, 0]
-            ) + max([1 * normalized_flux, 0])
+            a_e = self._almost_diffusion * max(
+                [(1 - 0.1 * abs(self._almost_flux / self._almost_diffusion)) ** 5, 0]
+            ) + max([-1 * self._almost_flux, 0])
+            a_w = self._almost_diffusion * max(
+                [(1 - 0.1 * abs(self._almost_flux / self._almost_diffusion)) ** 5, 0]
+            ) + max([1 * self._almost_flux, 0])
             # Density and velocity constant so in a_p last term cancels itself
             a_p = a_e + a_w
         else:
--- main.py
+++ main.py
@@ -37,37 +37,48 @@ phi_grid[-1] = 20
 system = CDS(phi_grid, scalars, length)
 
 # Solve
-an_sol = system.solve_analytically()
-an_sol_error = round(calc_error(an_sol, an_sol), 3)
+an_sol_achievable = False
+try:
+    an_sol = system.solve_analytically()
+    an_sol_error = round(calc_error(an_sol, an_sol), 3)
+    # Smooth out analytical solution for plotting
+    x_smooth = [x / 300 * length for x in range(0, 300)]
+    spl = make_interp_spline(x, an_sol, k=3)
+    an_sol_smooth = spl(x_smooth)
+    plt.plot(x, an_sol, "bo")
+    plt.plot(
+        x_smooth, an_sol_smooth, label="Analytical(Splined) - {}%".format(an_sol_error)
+    )
+    an_sol_achievable = True
+except OverflowError:
+    print("Cannot determine analytical solution as exponent too large")
 sol = dict()  # Dict of (solution,error,processing time)
 for scheme in DifferencingScheme:
     start = round(time.perf_counter_ns() / 1000)
     solution = system.solve_numerically(convection_scheme=scheme)
     end = round(time.perf_counter_ns() / 1000)
-    sol[scheme.name] = (solution, round(calc_error(an_sol, solution), 3), end - start)
-
-# Smooth out analytical solution for plotting
-x_smooth = [x / 300 * length for x in range(0, 300)]
-spl = make_interp_spline(x, an_sol, k=3)
-an_sol_smooth = spl(x_smooth)
+    if an_sol_achievable:
+        sol[scheme.name] = (
+            solution,
+            round(calc_error(an_sol, solution), 3),
+            end - start,
+        )
+    else:
+        sol[scheme.name] = (solution, "N/A ", end - start)
 
 # Display results
-plt.plot(x, an_sol, "bo")
-plt.plot(
-    x_smooth, an_sol_smooth, label="Analytical(Splined) - Err {}%".format(an_sol_error)
-)
 for scheme in DifferencingScheme:
     plt.plot(
         x,
         sol[scheme.name][0],
         "o--",
-        label="{} {}us - Err {}%".format(
+        label="{} {}us - {}% error".format(
             scheme.name.capitalize(), sol[scheme.name][2], sol[scheme.name][1]
         ),
     )
 plt.legend(loc="best")
 plt.title(
-    u"U = {}, ∆X = {} \n Γ = {}, ρ = {}, N = {} \n Global Pe = {}, Local Pe = {}".format(
+    u"U = {}, ∆X = {} \n Γ = {}, ρ = {}, N = {} \n Global Pe = {}, Local Pe = {}\n{}".format(
         scalars.velocity,
         length / nodes,
         scalars.diffusion_coefficient,
@@ -79,6 +90,7 @@ plt.title(
         * length
         / nodes
         / scalars.diffusion_coefficient,
+        "" if system.cd_stable else "Unstable for CD",
     )
 )
 plt.xlabel("X\nm")
